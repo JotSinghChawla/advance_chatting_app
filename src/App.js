5cbbde16378f197ef4b6a936441e83bc9b578567
Jot singh <jot.chawla.54.js@gmail.com>
2021-04-08 21:24:15 +0530

    Project is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
 import './App.css'
 
 function App() {
   return (
     <ChatEngine 
       height='100vh'
-      projectID='ProjectID'
-      userName='CurrentlyLoggedPerson'
-      userSecret=''
+      projectID='3659119a-1ea1-4d86-acf9-1ebc1b3ae6fa'
+      userName='random'
+      userSecret='random'
     />
   );
 }
